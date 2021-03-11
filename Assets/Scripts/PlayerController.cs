014deeab2f8a8361916a39a8a1cfd7f728899803
Justin Moore <theoperaking@yahoo.com>
2021-03-10 23:47:24 -0500

    Add basic movement script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D playerRigidbody;
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+        moveSpeed = 3f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+	}
+}
