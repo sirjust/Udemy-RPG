5da7f8328cc92cbffd36ebb57913d8cc69d5686b
Justin Moore <theoperaking@yahoo.com>
2021-08-11 21:21:59 -0400

    Use PlayerLoader to load player into scene

---
+++ Assets/Scripts/PlayerLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLoader : MonoBehaviour {
+
+	public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		if (PlayerController.instance == null) Instantiate(Player);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
