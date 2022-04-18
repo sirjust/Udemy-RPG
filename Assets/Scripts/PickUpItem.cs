bd8fa133f7591b1ab1ce03e7f54b51f29b282ddc
Justin Moore <theoperaking@yahoo.com>
2022-04-17 22:22:33 -0700

    Add pick up item functionality

---
+++ Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour {
+
+	private bool canPickUp;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(canPickUp && Input.GetButtonDown("Fire1") && PlayerController.instance.CanMove)
+        {
+			GameManager.instance.AddItem(GetComponent<Item>().itemName);
+			Destroy(gameObject);
+        }
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+		if(other.tag == "Player")
+        {
+			canPickUp = true;
+        }
+    }
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.tag == "Player")
+		{
+			canPickUp = false;
+		}
+	}
+}
