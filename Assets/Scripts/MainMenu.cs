95d7192e0734e1c205cf2f53ecbc331e3015e0bb
Justin Moore <theoperaking@yahoo.com>
2022-05-17 22:48:53 -0700

    Load game

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public string newGameScene;
+
+	public GameObject continueButton;
+
+	// Use this for initialization
+	void Start () {
+        if (PlayerPrefs.HasKey("Current_Scene"))
+        {
+			continueButton.SetActive(true);
+        }
+        else
+        {
+			continueButton.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Continue()
+    {
+
+    }
+
+	public void NewGame()
+    {
+		SceneManager.LoadScene(newGameScene);
+    }
+
+	public void Exit()
+    {
+		// We can't test this when running in Unity, only after game is built
+		Application.Quit();
+    }
+}
