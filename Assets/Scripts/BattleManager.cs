7c26637e9fbdda51a47371697d53aa5241270d29
sirjust <theoperaking@yahoo.com>
2022-05-21 16:17:11 -0700

    Fix camera for battle and walking around

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class BattleManager : MonoBehaviour {
 
@@ -13,6 +14,8 @@ public class BattleManager : MonoBehaviour {
 
 	public BattleChar[] playerPrefabs;
 	public BattleChar[] enemyPrefabs;
+
+	public List<BattleChar> activeBattlers = new List<BattleChar>();
 
 	// Use this for initialization
 	void Start () {
@@ -40,6 +43,22 @@ public class BattleManager : MonoBehaviour {
 			battleScene.SetActive(true);
 
 			AudioManager.instance.PlayBGM(0);
+
+            for (int i = 0; i < playerPositions.Length; i++)
+            {
+                if (GameManager.instance.PlayerStats[i].gameObject.activeInHierarchy)
+                {
+                    for (int j = 0; j < playerPrefabs.Length; j++)
+                    {
+						if(playerPrefabs[j].charName == GameManager.instance.PlayerStats[i].CharacterName)
+                        {
+							BattleChar newPlayer = Instantiate(playerPrefabs[j], playerPositions[i].position, playerPositions[i].rotation);
+							newPlayer.transform.parent = playerPositions[i];
+							activeBattlers.Add(newPlayer);
+                        }
+                    }
+                }
+            }
         }
     }
 }
