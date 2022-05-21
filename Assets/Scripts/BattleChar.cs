aabcbdf569a727ea2b0f0cc7effec7fb23da311c
sirjust <theoperaking@yahoo.com>
2022-05-21 14:52:28 -0700

    Add players and enemies to battle

---
+++ Assets/Scripts/BattleChar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BattleChar : MonoBehaviour {
+
+	public bool isPlayer;
+	public string[] movesAvailable;
+
+	public string charName;
+	public int currentHp, maxHp, currentMp, maxMp, strength, defense, weaponPower, armorPower;
+	public bool hasDied;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
